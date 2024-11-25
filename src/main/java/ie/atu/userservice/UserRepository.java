000914a870f1c50611321b64765a0c6f2d881f4d
G00402034 <G00402034@atu.ie>
2024-11-25 13:00:52 +0000

    added UserService to define all the methods used in this service and added user repository to interface with the db

--- src/main/java/ie/atu/userservice/UserRepository.java
+++ src/main/java/ie/atu/userservice/UserRepository.java
@@ -7,5 +7,5 @@ import java.util.Optional;
 @Repository
 public interface UserRepository extends MongoRepository<User, String> {
     Optional<User> findByEmail(String email);
-    Optional<User> findById(String id); // Already provided by MongoRepository
+    Optional<User> findById(String id);
 }
