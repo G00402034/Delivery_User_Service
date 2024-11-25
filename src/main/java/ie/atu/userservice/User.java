10ff0a705cc9c2dd6e3c906acc561caf5938e7d4
G00402034 <G00402034@atu.ie>
2024-11-25 13:00:20 +0000

    added UserService to define all the methods used in this service and added user repository to interface with the db

--- src/main/java/ie/atu/userservice/User.java
+++ src/main/java/ie/atu/userservice/User.java
@@ -1,11 +1,13 @@
 package ie.atu.userservice;
-
 import lombok.Data;
+import org.springframework.data.annotation.Id;
 import org.springframework.data.mongodb.core.mapping.Document;
 
-@Document(collection = "users")
 @Data
-public class User {
+@Document(collection = "users")
+public class User
+{
+    @Id
     private String id;
     private String username;
     private String password;
---
+++ src/main/java/ie/atu/userservice/UserService.java
@@ -0,0 +1,62 @@
+package ie.atu.userservice;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.Optional;
+
+@Service
+public class UserService {
+
+    @Autowired
+    private UserRepository userRepository;
+
+
+    public User createUser(User user) {
+        Optional<User> existingUser = userRepository.findByEmail(user.getEmail());
+        if (existingUser.isPresent()) {
+            throw new RuntimeException("User with this email already exists.");
+        }
+        return userRepository.save(user);
+    }
+
+
+    public User updateUser(String userId, User updatedUser) {
+        Optional<User> userOptional = userRepository.findById(userId);
+        if (userOptional.isPresent()) {
+            User user = userOptional.get();
+            user.setUsername(updatedUser.getUsername());
+            user.setEmail(updatedUser.getEmail());
+            user.setPhoneNumber(updatedUser.getPhoneNumber());
+            user.setAddress(updatedUser.getAddress());
+            return userRepository.save(user);
+        }
+        throw new RuntimeException("User not found");
+    }
+
+
+    public User getUserById(String userId) {
+        return userRepository.findById(userId).orElseThrow(() -> new RuntimeException("User not found"));
+    }
+
+
+    public void deactivateUser(String userId) {
+        Optional<User> userOptional = userRepository.findById(userId);
+        if (userOptional.isPresent()) {
+            User user = userOptional.get();
+            user.setActive(false);
+            userRepository.save(user);
+        } else {
+            throw new RuntimeException("User not found");
+        }
+    }
+
+    public User authenticate(String email, String password) {
+        Optional<User> userOptional = userRepository.findByEmail(email);
+        if (userOptional.isPresent() && userOptional.get().getPassword().equals(password)) {
+            return userOptional.get();
+        }
+        throw new RuntimeException("Invalid credentials");
+    }
+}
+
